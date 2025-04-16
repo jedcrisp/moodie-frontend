160008838091f21375106c1a81e0b69f8c88770e
jedcrisp <59922178+jedcrisp@users.noreply.github.com>
2025-04-15 20:04:20 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import MoodFlow from './components/MoodFlow';
+import MoodFlow from './components/MoodSelector';
 import AdminDashboard from './components/AdminDashboard';
 import SignIn from './components/SignIn';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
